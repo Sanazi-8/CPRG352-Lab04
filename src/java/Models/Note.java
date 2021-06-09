9a2019805b7deaf5033fd8566be5828dd9b5d84d
850354 <850354@SAIT227242IT.ACDM.DS.SAIT.CA>
2021-06-09 01:05:00 -0600

    added code to my note java class instantiated variables and set the attributes and added get methods.

--- src/java/Models/Note.java
+++ src/java/Models/Note.java
@@ -1,10 +1,40 @@
 
 package Models;
 
+import java.io.Serializable;
+
 /**
  *
  * @author 850354
  */
-public class Note {
+public class Note implements Serializable {
+   
+    
+    //instanstiate your variables and make sure they are thread safe
+    private String userTitle;
+    private String userContent;
+    
+    
+    
+    
+    public Note(String userTitle, String userContent){
+        this.userTitle = userTitle;
+        this.userContent = userContent;   
+    }
+    // set getters and setters for each parameter 
+    public String getUserTitle() {
+    return userTitle;
+}
+    //set the parameter using void 
+    public void setUserTitle(String userTitle){
+    this.userTitle= userTitle;
+}
+     public String getUserContent() {
+    return userContent;
+}
+      public void setUserContent(String userContent){
+    this.userContent= userContent;
+}
+      
     
 }
