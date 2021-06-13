1d27e6c700ceb3d81d473206f8b9ab76b25c1a82
850354 <850354@SAIT227242IT.ACDM.DS.SAIT.CA>
2021-06-13 13:33:07 -0600

    Finished the DoGet method i intaked the text from the notes txt I created at first so that it would show in the boxes for the user. I used if statements to intake the user's action and according to that show the page they wish.

--- src/java/Servlets/NoteServlet.java
+++ src/java/Servlets/NoteServlet.java
@@ -5,6 +5,7 @@
  */
 package Servlets;
 
+import Models.Note;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -26,6 +27,7 @@ public class NoteServlet extends HttpServlet {
        //this method gets the path for the notes txt we have made
        String noteFilePath = getServletContext().getRealPath("/WEB-INF/note.txt");
        
+       
        //by declaring this method learned we are preaparing the system to 
        //to read the content in the file
        
@@ -37,9 +39,38 @@ public class NoteServlet extends HttpServlet {
        String content ="";
        
        // now we read the lines from the notes.txt
-       title = title+ readFile.readLine();
+       title = title + readFile.readLine();
        content = content + readFile.readLine();
-               
+       //declare the note 
+       Note note = new Note (title, content);
+       //remember to stop the buffered file
+       readFile.close();
+       
+       
+       //set the attribute (notes)
+       request.setAttribute("note", note);
+       
+       //now we are getting the edit button 
+       String editButton = request.getParameter("edit");
+       
+       //if else statements to intake the input of the user
+       //of wether they clicked the edit button or not
+       
+       if(editButton !=null){
+         // in this condition the user has clicked the button
+         //so we display the edit page 
+         getServletContext().getRequestDispatcher("/WEB-INF/editnote.jsp").forward(request, response);
+       }
+       else{
+           //in this condition the user has not clicked edit
+           //so the program shows the view page
+           getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
+       }
+       
+       
                }
+   
+   
+   
 
 }
